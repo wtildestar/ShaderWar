a990ff0a34d5575ced53d369e9441fad6696a605
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-12-02 13:42:54 +0500

    Initial Commit

---
+++ ShaderWar/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  ShaderWar
+//
+//  Created by wtildestar on 02/12/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        let sprite = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: 100))
+        sprite.position = CGPoint(x: 200, y: 200)
+        self.addChild(sprite)
+        
+    }
+}
---
+++ ShaderWar/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  ShaderWar
+//
+//  Created by wtildestar on 02/12/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+                
+            let scene = GameScene(size: self.view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        
+        return .portrait
+        
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
